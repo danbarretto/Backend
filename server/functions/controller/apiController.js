d35872f5ce243e4c00bbbd55c66de35f3b59f589
danbarretto <daniel_barretto98@hotmail.com>
2020-11-25 13:26:35 -0300

    Add cripto currency routes

---
+++ server/functions/controller/apiController.js
@@ -0,0 +1,54 @@
+const axios = require('axios').default
+const express = require('express')
+const router = express.Router()
+const verifyMiddleware = require('../middleware/verifyToken')
+const apiKey = require('../config/apikey.json')
+const config = {
+  headers: {
+    authorization: apiKey.key,
+  },
+}
+//router.use(verifyMiddleware)
+
+router.get('/topList', (req, res) => {
+  axios
+    .get(
+      'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=BRL',
+      config
+    )
+    .then((result) => {
+      const topList = result.data.Data.map((currency) => {
+        //console.log(currency)
+        return {
+          name: currency.CoinInfo.Name,
+          fullName: currency.CoinInfo.FullName,
+          price: currency.DISPLAY.BRL.PRICE,
+        }
+      })
+      return res.send({topList})
+    })
+})
+
+router.get('/searchCurrency', (req, res)=>{
+    const {coinName} = req.query
+    axios.get(`https://min-api.cryptocompare.com/data/price?fsym=${coinName}&tsyms=BRL`, config)
+    .then(price=>{
+        if(price.data.Response === 'Error') res.status(404).send({message:'Moeda não encontrada'})
+        else res.send({price:price.data.BRL})
+    }).catch(err=>{
+        res.status(400).send({message:'Erro ao pesquisar moeda!', err})
+    })
+    
+})
+
+router.get('/historicalData', (req, res)=>{
+    const {coinName, time} = req.query
+    axios.get(`https://min-api.cryptocompare.com/data/v2/histoday?fsym=${coinName}&tsym=BRL&limit=${time}`, config)
+    .then(result=>{
+        res.send(result.data.Data)
+    }).catch(err=>{
+        res.send(err)
+    })
+})
+
+module.exports = (app) => app.use('/api', router)
--- server/functions/controller/userController.js
+++ server/functions/controller/userController.js
@@ -2,19 +2,55 @@ const express = require('express')
 const router = express.Router()
 const admin = require('./admin')
 const verifyMiddleware = require('../middleware/verifyToken')
+const { user } = require('firebase-functions/lib/providers/auth')
+
 router.use(verifyMiddleware)
-router.post('/addUserToDb', async(req, res)=>{
-  const {uid, userName} = req.body
+
+router.post('/addUserToDb', async (req, res) => {
+  const { userName } = req.body
+  const { uid } = req.user.uid
   console.log(req.body)
-  admin.firestore().collection('users').doc(uid).set({
-    userName
-  }).then(()=>{
-    return res.send({message:'Usuário criado com sucesso!'})
-  }).catch(err=>{
-    return res.status(400).send({message:`Erro ao criar usuário! ${err.message}`})
-  })
+  admin
+    .firestore()
+    .collection('users')
+    .doc(uid)
+    .set({
+      userName,
+    })
+    .then(() => {
+      return res.send({ message: 'Usuário criado com sucesso!' })
+    })
+    .catch((err) => {
+      return res
+        .status(400)
+        .send({ message: `Erro ao criar usuário! ${err.message}` })
+    })
 })
 
-
+router.post('/addCryptoCurrency', async (req, res) => {
+  const { currencies } = req.body
+  const { uid } = req.user.uid
+  const userRef = admin.firestore().collection('users').doc(uid)
+  const getDoc = await userRef.get()
+  try {
+    if (getDoc.exists) {
+      if (getDoc.get('currencies') === null) {
+        await userRef.set({ currencies })
+      } else {
+        let oldCurrencies = getDoc.get('currencies')
+        currencies.forEach((currency) => {
+          const { currencyName, qtd } = currency
+          oldCurrencies[currencyName] = qtd
+        })
+        await userRef.set({ currencies: oldCurrencies })
+      }
+      return res.sendStatus(200)
+    }
+  } catch (err) {
+    return res
+      .status(400)
+      .send({ message: 'Erro ao atualizar cripto moedas! ' + err.message })
+  }
+})
 
 module.exports = (app) => app.use('/user', router)
